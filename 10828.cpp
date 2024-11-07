4ce51c91bd04d8eb60a8ba94d4900325aeebd93f
juhun_park <juhun_park@tmax.co.kr>
2024-11-07 16:08:24 +0900

    [tmp]

---
+++ 10828.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 알고리즘 : 스택
+// [문제요약]
+// 정수를 저장하는 스택을 구현
+int main() {
+  stack<int> s;
+  int n;
+  string command;
+
+  cin >> n;
+  for (int i = 0; i<n; i++) {
+    cin >> command;
+    if (command == "push") {
+      int 
+      cin >> s
+    } else if (command == "pop") {
+
+    } else if (command == "size") {
+
+    } else if (command == "empty") {
+
+    } else if (command == "top") {
+
+    }
+  }
+
+}
